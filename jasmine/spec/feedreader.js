9ee910513abd0c130651d8ebed7aa19991988874
stivenlancheros <38940144+stivenlancheros@users.noreply.github.com>
2018-07-31 21:32:12 +0200

    loop for AllFeeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,13 +31,18 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('has a defined url', function() {
+                     allFeeds.forEach(function(feed) {
+                         expect(feed.url).toBeDefined();
+                         expect(feed.url.length).not.toBe(0);
+                     });
+                 });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-    });
+
 
 
     /* TODO: Write a new test suite named "The menu" */
